22c1f0ecf8151e48876ccb0d578de964edfdf76a
Adigun Oreoluwa <118155449+Adigun-Oreoluwa@users.noreply.github.com>
2025-05-18 13:37:54 -0700

    work pls

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {port:5174} 
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 5174, // use Render-assigned port or fallback
+    host: '0.0.0.0', // required so Render can access the server
+  }
 })
